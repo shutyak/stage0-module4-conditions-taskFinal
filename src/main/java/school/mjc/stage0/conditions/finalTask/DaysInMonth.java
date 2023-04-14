1497d5e9380c88106c443e5f352be89e7e4949aa
vlshutyak <vl.shutyak@gmail.com>
2023-04-14 23:07:50 +0300

    s0-m4-final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,45 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
+    private boolean isALeap(int year) {
+        boolean isLeap = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeap = true;
+                }
+            } else {
+                isLeap = true;
+            }
+        }
+        return isLeap;
+    }
+
     public void printDays(int year, int month) {
+        switch (month) {
+            default:
+                System.out.println("invalid date");
+                break;
+            case 2:
+                if (isALeap(year)) System.out.println(29);
+                else System.out.println(28);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result;
+        if (divider == 0) {
+            System.out.println("division by zero");
+            return;
+        } else {
+            result = dividend / divider;
+        }
+        int product = result * divider;
+        if (product == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
     }
 }
